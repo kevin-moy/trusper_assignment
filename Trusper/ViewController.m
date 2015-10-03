c13923fb121a325be478b37021a6ff65968219b5
Kevin <kevintmoy@gmail.com>
2015-10-03 14:43:59 -0700

    first comment with cocoapods

---
+++ Trusper/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Trusper
+//
+//  Created by Kevin Moy on 10/3/15.
+//  Copyright © 2015 Kevin Moy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
